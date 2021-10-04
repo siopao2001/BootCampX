bbca40375d5a8f1b37d2d0ae743eb5320a9fb5f2
siopao2001 <sio_pao2001@yahoo.ca>
2021-10-04 19:26:18 +0000

    Add 3_queries_Group-by

---
+++ 3_queries_group-by/low_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY AVG(assignment_submissions.duration);
